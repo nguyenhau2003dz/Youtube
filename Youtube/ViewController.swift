662101931cabf044a96f2507f3f28993545c6feb
nguyenhau2003dz <nguyenduchaudeptrai2003@gmail.comnguyenduchaudeptrai2003@gmail.comnguyenduchaudeptrai2003@gmail.com>
2025-04-26 12:09:56 +0700

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Hậu Nguyễn on 26/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YoutubeTests/YoutubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeTests.swift
+//  YoutubeTests
+//
+//  Created by Hậu Nguyễn on 26/4/25.
+//
+
+import Testing
+@testable import Youtube
+
+struct YoutubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
